ef106616bb6104a0746acc4c54db798dc7822188
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-10 04:44:21 +0300

    add food,tech,tavel and sports view functions

--- photoapp/views.py
+++ photoapp/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+# Create your views here.
 def welcome(request):
     return render(request,'welcome.html')
 
-# Create your views here.
+def food(request):
+    # date = dt.date.today()
+    return render(request, 'all-news/food.html')
+def sports(request):
+    # date = dt.date.today()
+    return render(request, 'all-news/sports.html')
+def tech(request):
+    # date = dt.date.today()
+    return render(request, 'all-news/tech.html')
+
+def travel(request):
+    # date = dt.date.today()
+    return render(request, 'all-news/travel.html')    

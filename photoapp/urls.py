da8b168d5449af986eb2a68555e68d2ad70242d0
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-10 05:19:23 +0300

    add navbar and url patterns for food,sports,tech and travel

--- photoapp/urls.py
+++ photoapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url(r'^$',views.food,name='food'),
+    url(r'^$',views.sports,name='sports'),
+    url(r'^$',views.tech,name='tech'),
+    url(r'^$',views.travel,name='travel'),
 ]

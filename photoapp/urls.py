aa69b3eb93d01df9177819943ed52dce538e54e7
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-11 04:35:00 +0300

    add image file in all photos

--- photoapp/urls.py
+++ photoapp/urls.py
@@ -2,9 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    # url('^$',views.welcome,name = 'welcome'),
-    url(r'^$',views.food,name='food'),
-    url(r'^$',views.sports,name='sports'),
-    url(r'^$',views.tech,name='tech'),
-    url(r'^$',views.travel,name='travel')
+    url(r'^$',views.category,name='category'),
+    
 ]

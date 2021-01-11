16ee8aba34cc3fb66786ee66f92cf9bb4ecc3f7a
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-11 05:06:07 +0300

    add photo model

--- photoapp/models.py
+++ photoapp/models.py
@@ -78,4 +78,22 @@ class Location(models.Model):
         locate = Location.objects.get(pk = id)
         return locate
     def __str__(self):
-        return self.photo_location                  
+        return self.photo_location 
+
+class Photographer(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.first_name
+    def save_photographer(self):
+        self.save()
+    def delete_photographer(self):
+        self.delete()   
+    @classmethod
+    def display_photographers(cls):
+        photographers= Photographer.objects.all()
+        for photographer in photographers:
+            return photographer        
+    class Meta:
+        ordering = ['first_name']                         

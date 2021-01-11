0ed706d0010670f4b26856b8f657d4f325204206
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-11 05:05:09 +0300

    add location model

--- photoapp/models.py
+++ photoapp/models.py
@@ -57,4 +57,25 @@ class Category(models.Model):
         return category
 
     def __str__(self):
-        return self.photo_category         
+        return self.photo_category   
+
+class Location(models.Model):
+    photo_location = models.CharField(max_length =30)
+
+    
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
+    
+    def update_location(self, update):
+        self.photo_location = update
+        self.save()  
+
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+    def __str__(self):
+        return self.photo_location                  

6e057eb8c3ac2eaf07e1f17c359cf432315a8402
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-11 05:22:52 +0300

    add test cases and instances

--- photoapp/models.py
+++ photoapp/models.py
@@ -1,13 +1,73 @@
 from django.db import models
-
+import datetime as dt
 # Create your models here.
+class Photographer(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    
+    def __str__(self):
+        return self.first_name
+    def save_photographer(self):
+        self.save()
+    def delete_photographer(self):
+        self.delete()   
+    @classmethod
+    def display_photographers(cls):
+        photographers= Photographer.objects.all()
+        for photographer in photographers:
+            return photographer        
+    class Meta:
+        ordering = ['first_name'] 
+
+class Location(models.Model):
+    photo_location = models.CharField(max_length =30)
+
+    
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
+    
+    def update_location(self, update):
+        self.photo_location = update
+        self.save()  
+
+    @classmethod
+    def get_location_id(cls, id):
+        locate = Location.objects.get(pk = id)
+        return locate
+    def __str__(self):
+        return self.photo_location 
+
+class Category(models.Model):
+    photo_category = models.CharField(max_length =30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+    
+    def update_category(self, update):
+        self.photo_category = update
+        self.save()
+    def get_category_id(cls,id):
+        category = Category.object.get(pk = id)
+        return category
+
+    def __str__(self):
+        return self.photo_category   
+
+
+
 class Image(models.Model):
     title = models.CharField(max_length =30)
-    photographer = models.ForeignKey(Photographer)
+    photographer = models.ForeignKey(Photographer, on_delete=models.CASCADE)
     description = models.TextField(max_length =30)
     image = models.ImageField(upload_to = 'photos/', default='No image')
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True, null=True) 
 
     class Meta:
@@ -40,60 +100,3 @@ class Image(models.Model):
        images = Image.objects.filter(location_id=id)
        return images 
 
-class Category(models.Model):
-    photo_category = models.CharField(max_length =30)
-
-    def save_category(self):
-        self.save()
-
-    def delete_category(self):
-        self.delete()
-    
-    def update_category(self, update):
-        self.photo_category = update
-        self.save()
-    def get_category_id(cls,id):
-        category = Category.object.get(pk = id)
-        return category
-
-    def __str__(self):
-        return self.photo_category   
-
-class Location(models.Model):
-    photo_location = models.CharField(max_length =30)
-
-    
-    def save_location(self):
-        self.save()
-    
-    def delete_location(self):
-        self.delete()
-    
-    def update_location(self, update):
-        self.photo_location = update
-        self.save()  
-
-    @classmethod
-    def get_location_id(cls, id):
-        locate = Location.objects.get(pk = id)
-        return locate
-    def __str__(self):
-        return self.photo_location 
-
-class Photographer(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    
-    def __str__(self):
-        return self.first_name
-    def save_photographer(self):
-        self.save()
-    def delete_photographer(self):
-        self.delete()   
-    @classmethod
-    def display_photographers(cls):
-        photographers= Photographer.objects.all()
-        for photographer in photographers:
-            return photographer        
-    class Meta:
-        ordering = ['first_name']                         
--- photoapp/tests.py
+++ photoapp/tests.py
@@ -1,3 +1,131 @@
 from django.test import TestCase
+from .models import Photographer,Location,Image,Category
+import datetime as dt
+
+# Create your tests here.
+class PhotographerTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.hannah= Photographer(first_name = 'Hannah', last_name ='Njeri')
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.hannah,Photographer))   
+    # Testing Save Method
+    def test_save_method(self):
+        self.hannah.save_photographer()
+        photographers = Photographer.objects.all()
+        self.assertTrue(len(photographers) > 0) 
+    def test_delete_method(self):
+        self.hannah.save_photographer()
+        self.hannah.delete_photographer()
+        photographers = Photographer.objects.all()
+        self.assertTrue(len(photographers) == 0) 
+    def test_display_method(self):
+        self.hannah.save_photographer()
+        self.hannah.display_photographers()
+        photographers = Photographer.objects.all()
+        self.assertTrue(len(photographers) > 0) 
+    def test_update_method(self):
+        self.hannah.save_photographer()
+        new_photographer = Photographer.objects.filter(first_name="Hannah").update(first_name="mercy")
+        photographers=Photographer.objects.get(first_name="mercy")
+        self.assertTrue(photographers.first_name,"mercy")           
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.nairobi = Location(photo_location='Kenya')
+        self.nairobi.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+    
+    def test_updating_location(self):
+        location = Location.get_location_id(self.nairobi.id)
+        location.update_location('Italy')
+        location = Location.get_location_id(self.nairobi.id)
+        self.assertTrue(location.photo_location == 'Italy')
+    
+    def tearDown(self):
+        self.nairobi.delete_location() 
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.ladieswear = Category(photo_category='Menswear')
+        self.ladieswear.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.ladieswear,Category))
+    
+    def tearDown(self):
+        self.ladieswear.delete_category()
+    
+    
+class ImageTestClass(TestCase):
+   """
+   Tests Image class and its functions
+   """
+   #Set up method
+   def setUp(self):
+        self.menswear = Category(photo_category='Menswear')
+        self.menswear.save_category()
+
+        self.nairobi = Location(photo_location='Nairobi')
+        self.nairobi.save_location()
+
+        self.image = Image(title='Fashion', description='menswear', location=self.nairobi, category=self.menswear)
+        self.image.save_image()
+   def test_instance(self):
+       self.assertTrue(isinstance(self.image, Image))
+
+   def test_save_method(self):
+       """
+       Function to test an image and its details is being saved
+       """
+       self.image.save_image()
+       images = Image.objects.all()
+       self.assertTrue(len(images) > 0)
+
+   def test_delete_method(self):
+       """
+       Function to test if an image can be deleted
+       """
+       self.image.save_image()
+       self.image.delete_image()
+
+   def test_update_method(self):
+       """
+       Function to test that an image's details can be updates
+       """
+       self.image.save_image()
+       new_image = Image.objects.filter(photo='image1.jpg').update(photo='download.jpg')
+       images = Image.objects.get(photo='download.jpg')
+       self.assertTrue(images.photo, 'download.jpg')
+
+   def test_get_image_by_id(self):
+       """
+       Function to test if you can get an image by its id
+       """
+       self.image.save_image()
+       this_img= self.image.get_image_by_id(self.image.id)
+       image = Image.objects.get(id=self.image.id)
+       self.assertTrue(this_img, image)
+
+   def test_filter_by_location(self):
+       """
+       Function to test if you can get an image by its location
+       """
+       self.image.save_image()
+       this_img = self.image.filter_by_location(self.image.location_id)
+       image = Image.objects.filter(location=self.image.location_id)
+       self.assertTrue(this_img, image)
+
+   def test_filter_by_category_name(self):
+       """
+       Function to test if you can get an image by its category name
+       """
+       self.image.save_image()
+       images = Image.search_image('this')
+       self.assertTrue(len(images)>0)
+          
 
 # Create your tests here.

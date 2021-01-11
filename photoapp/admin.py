157246b2e94f0a2817eab89006138656cfef249d
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-11 05:25:08 +0300

    add admin site for models

--- photoapp/admin.py
+++ photoapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Photographer,Location,Image,Category
 # Register your models here.
+
+
+admin.site.register(Photographer)
+admin.site.register(Location)
+admin.site.register(Image)
+admin.site.register(Category)

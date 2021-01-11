458207a08d730011646119e51df6db6d4674bf93
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-11 05:33:53 +0300

    add search option

--- photoapp/views.py
+++ photoapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404# responsible for returning a response to a user
+import datetime as dt
+from .models import Image,Location,Photographer,Category
 
 # Create your views here.
 def welcome(request):
@@ -7,3 +10,16 @@ def welcome(request):
 def category(request):
     images = Image.objects.all()
     return render(request, 'category.html',{'images':images})
+
+def search_results(request):
+    
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_photos = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-photo/search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-photo/search.html',{"message":message})    

40f624174f6693d8d7be2a66f1eed533120e6577
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-11 05:39:58 +0300

    add search url

--- photoapp/urls.py
+++ photoapp/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    url(r'^$',views.category,name='category'),
-    
+    url(r'^$',views.category,name = 'category'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.image,name ='image'),
+    url(r'location/(\d+)',views.filter_by_location,name='location')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

3c85bdee10ac270293bed3e3091987c096e95379
Cynthia Kitili <nyambucindy0@gmail.com>
2021-01-12 14:01:02 +0300

    deployment to heroku

--- photo/settings.py
+++ photo/settings.py
@@ -171,7 +171,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
 
 
 cloudinary.config( 
